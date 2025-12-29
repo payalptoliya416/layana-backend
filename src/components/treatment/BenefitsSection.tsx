59027699f627c0268af858eecdffa4fe4d32a702
payalptoliya416 <payal.rising416@gmail.com>
2025-12-29 16:24:51 +0530

    sas

--- src/components/treatment/BenefitsSection.tsx
+++ src/components/treatment/BenefitsSection.tsx
@@ -226,7 +226,7 @@ const isFacial = category === "Facial";
       <button
         onClick={addBenefit}
         className="
-          flex h-[34px] w-[50px] items-center justify-center
+          flex h-[34px] w-full xl:w-[50px] items-center justify-center
           rounded-full
           bg-primary
           text-primary-foreground
