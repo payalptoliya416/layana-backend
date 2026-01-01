9cace111e5bc092a80bb6439d76ac5f768cb6e91
payalptoliya416 <payal.rising416@gmail.com>
2026-01-01 15:14:13 +0530

    sas

--- src/components/layout/massageMembership/MemberNav.tsx
+++ src/components/layout/massageMembership/MemberNav.tsx
@@ -53,14 +53,9 @@ export function MemberNav({ activeItem, onItemChange }: SecondaryNavProps) {
               onClick={() => onItemChange(item.id)}
               className={cn(
                 "flex items-center gap-3 px-2 sm:px-5 py-2 lg:py-4 rounded-md lg:rounded-[16px] text-sm transition-all whitespace-nowrap",
+                 "last:pr-6 lg:last:pr-2", 
                 isActive
-                  ? `
-        bg-card
-        border border-primary
-        text-primary  font-medium
-         greenshadow
-      `
-                  : "text-muted-foreground hover:bg-muted"
+                  ? `  bg-card border border-primary text-primary font-medium  greenshadow ` : "text-muted-foreground hover:bg-muted"
               )}
             >
               <Icon
