a4249b5282a9e20fc5ed574351ae8b8487ad647a
payalptoliya416 <payal.rising416@gmail.com>
2026-01-02 10:21:11 +0530

    sas

--- src/hooks/useAutoRowsmembership.tsx
+++ src/hooks/useAutoRowsmembership.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
-const HEADER_HEIGHT = 46;
-const FIXED_ROW_HEIGHT = 6; 
+const HEADER_HEIGHT = 52;
+const FIXED_ROW_HEIGHT = 66; 
 
 export function useAutoRowsmembership() {
   const containerRef = useRef<HTMLDivElement | null>(null);
