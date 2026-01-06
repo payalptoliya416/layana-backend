d5964e2e60d338db1be074144e34163d493a71cb
payalptoliya416 <payal.rising416@gmail.com>
2026-01-06 17:24:04 +0530

    test

--- src/components/terms&condition/TermsDescriptionEditor.tsx
+++ src/components/terms&condition/TermsDescriptionEditor.tsx
@@ -1,36 +1,61 @@
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { boolean } from "zod";
 
 interface DescriptionEditorProps {
   value: string;
   onChange: (value: string) => void;
-  fullHeight?: boolean;
+    fullHeight?: boolean;
 }
 
 export default function TermsDescriptionEditor({
   value,
   onChange,
-  fullHeight = false,
+     fullHeight = false,
 }: DescriptionEditorProps) {
+  const modules = {
+    toolbar: [
+      [{ font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ color: [] }, { background: [] }],
+      [{ script: "super" }, { script: "sub" }],
+      [{ header: 1 }, { header: 2 }],
+      ["blockquote", "code-block"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ indent: "-1" }, { indent: "+1" }],
+      [{ align: [] }],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
   return (
-    <div className={`flex flex-col ${fullHeight ? "h-full" : ""}`}>
-      <div
-        className="
-          flex-1
-          rounded-[10px]
-          border border-border
-          bg-card
-          overflow-hidden
-        "
-      >
-        <ReactQuill
-          theme="snow"
-          value={value}
-          onChange={onChange}
-          placeholder="Enter Description"
-          className="h-full text-foreground"
-        />
-      </div>
+ <div className="">
+ <div
+    className="
+      rounded-[10px]
+      border border-border
+      bg-card
+      overflow-hidden
+      transition
+      focus-within:ring-2
+      focus-within:ring-ring/20
+      focus-within:border-ring
+    " 
+      style={fullHeight ? { height: "100%" } : { minHeight: "260px" }}
+  >
+    <div className="">
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={onChange}
+      modules={modules}
+      placeholder="Enter Description"
+      className="h-full text-foreground"
+    />
     </div>
+  </div>
+</div>
   );
 }
