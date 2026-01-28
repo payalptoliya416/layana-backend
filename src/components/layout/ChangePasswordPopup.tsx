605617a729444a18397c08f215bf65405c15e2c1
payalptoliya416 <payal.rising416@gmail.com>
2026-01-28 17:52:50 +0530

    changes

--- src/components/layout/ChangePasswordPopup.tsx
+++ src/components/layout/ChangePasswordPopup.tsx
@@ -11,13 +11,13 @@ export default function ChangePasswordPopup({ onClose }: any) {
     new_password: "",
     confirm_password: "",
   });
-const [message, setMessage] = useState<{
-  type: "success" | "error" | "";
-  text: string;
-}>({
-  type: "",
-  text: "",
-});
+  const [message, setMessage] = useState<{
+    type: "success" | "error" | "";
+    text: string;
+  }>({
+    type: "",
+    text: "",
+  });
 
   const [fieldErrors, setFieldErrors] = useState<any>({});
   const [showPassword, setShowPassword] = useState({
@@ -26,13 +26,13 @@ const [message, setMessage] = useState<{
     confirm: false,
   });
   const [loading, setLoading] = useState(false);
-const showMessage = (type: "success" | "error", text: string) => {
-  setMessage({ type, text });
+  const showMessage = (type: "success" | "error", text: string) => {
+    setMessage({ type, text });
 
-  setTimeout(() => {
-    setMessage({ type: "", text: "" });
-  }, 3000); // 3 sec
-};
+    setTimeout(() => {
+      setMessage({ type: "", text: "" });
+    }, 3000); // 3 sec
+  };
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
@@ -78,17 +78,19 @@ const showMessage = (type: "success" | "error", text: string) => {
 
       const res = await changePassword(formData);
 
-     showMessage("success", res?.message || "Password updated successfully!");
+      showMessage("success", res?.message || "Password updated successfully!");
 
-  setTimeout(() => {
-    onClose();
-  }, 2000);
-
+      setTimeout(() => {
+        onClose();
+      }, 2000);
     } catch (error: any) {
       if (error?.response?.data?.errors) {
         setFieldErrors(error.response.data.errors);
       } else {
-       showMessage("error", error?.response?.data?.message || "Something went wrong!");
+        showMessage(
+          "error",
+          error?.response?.data?.message || "Something went wrong!",
+        );
       }
     } finally {
       setLoading(false);
@@ -110,142 +112,150 @@ const showMessage = (type: "success" | "error", text: string) => {
         </h2>
 
         <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
+          <div>
             <label className="text-sm font-medium text-foreground">
-      Current Password <span className="text-red-500">*</span>
-    </label>
-         <div className="relative">
-          
-  <input
-    type={showPassword.current ? "text" : "password"}
-    name="current_password"
-    placeholder="Current Password"
-    value={formData.current_password}
-    onChange={handleChange}
-      className={`form-input pr-10 ${
-    fieldErrors.current_password ? "border border-red-500 focus:ring-red-500" : ""
-  }`}
-  />
+              Current Password <span className="text-red-500">*</span>
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword.current ? "text" : "password"}
+                name="current_password"
+                placeholder="Current Password"
+                value={formData.current_password}
+                onChange={handleChange}
+                className={`form-input pr-10 ${
+                  fieldErrors.current_password
+                    ? "border border-red-500 focus:ring-red-500"
+                    : ""
+                }`}
+              />
 
-  {/* Eye Button */}
-  <button
-    type="button"
-    onClick={() =>
-      setShowPassword((prev) => ({
-        ...prev,
-        current: !prev.current,
-      }))
-    }
-    className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground"
-  >
-    {showPassword.current ? <EyeOff size={18} /> : <Eye size={18} />}
-  </button>
+              {/* Eye Button */}
+              <button
+                type="button"
+                onClick={() =>
+                  setShowPassword((prev) => ({
+                    ...prev,
+                    current: !prev.current,
+                  }))
+                }
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground"
+              >
+                {showPassword.current ? (
+                  <EyeOff size={18} />
+                ) : (
+                  <Eye size={18} />
+                )}
+              </button>
 
-  {/* Error */}
-         </div>
-          {fieldErrors.current_password && (
-            <p className="text-red-500 text-xs mt-1">
-              {Array.isArray(fieldErrors.current_password)
-                ? fieldErrors.current_password[0]
-                : fieldErrors.current_password}
-            </p>
-          )}
+              {/* Error */}
             </div>
-
+            {fieldErrors.current_password && (
+              <p className="text-red-500 text-xs mt-1">
+                {Array.isArray(fieldErrors.current_password)
+                  ? fieldErrors.current_password[0]
+                  : fieldErrors.current_password}
+              </p>
+            )}
+          </div>
 
           {/* New Password */}
           <div className="">
-           <label className="text-sm font-medium text-foreground">
-      New Password <span className="text-red-500">*</span>
-    </label>
-        <div className="relative">
-  <input
-    type={showPassword.new ? "text" : "password"}
-    name="new_password"
-    placeholder="New Password"
-    value={formData.new_password}
-    onChange={handleChange}
-    className={`form-input pr-10 ${
-    fieldErrors.current_password ? "border border-red-500 focus:ring-red-500" : ""
-  }`}
-  />
+            <label className="text-sm font-medium text-foreground">
+              New Password <span className="text-red-500">*</span>
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword.new ? "text" : "password"}
+                name="new_password"
+                placeholder="New Password"
+                value={formData.new_password}
+                onChange={handleChange}
+                className={`form-input pr-10 ${
+                  fieldErrors.new_password
+                    ? "border border-red-500 focus:ring-red-500"
+                    : ""
+                }`}
+              />
 
-  <button
-    type="button"
-    onClick={() =>
-      setShowPassword((prev) => ({
-        ...prev,
-        new: !prev.new,
-      }))
-    }
-    className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground"
-  >
-    {showPassword.new ? <EyeOff size={18} /> : <Eye size={18} />}
-  </button>
-
-</div>
-  {fieldErrors.new_password && (
-    <p className="text-red-500 text-xs mt-1">
-      {Array.isArray(fieldErrors.new_password)
-        ? fieldErrors.new_password[0]
-        : fieldErrors.new_password}
-    </p>
-  )}
+              <button
+                type="button"
+                onClick={() =>
+                  setShowPassword((prev) => ({
+                    ...prev,
+                    new: !prev.new,
+                  }))
+                }
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground"
+              >
+                {showPassword.new ? <EyeOff size={18} /> : <Eye size={18} />}
+              </button>
+            </div>
+            {fieldErrors.new_password && (
+              <p className="text-red-500 text-xs mt-1">
+                {Array.isArray(fieldErrors.new_password)
+                  ? fieldErrors.new_password[0]
+                  : fieldErrors.new_password}
+              </p>
+            )}
           </div>
 
           {/* Confirm Password */}
           <div className="">
-             <label className="text-sm font-medium text-foreground">
-       Confirm Password <span className="text-red-500">*</span>
-    </label>
-       <div className="relative">
-  <input
-    type={showPassword.confirm ? "text" : "password"}
-    name="confirm_password"
-    placeholder="Confirm Password"
-    value={formData.confirm_password}
-    onChange={handleChange}
-      className={`form-input pr-10 ${
-    fieldErrors.current_password ? "border border-red-500 focus:ring-red-500" : ""
-  }`}
-  />
+            <label className="text-sm font-medium text-foreground">
+              Confirm Password <span className="text-red-500">*</span>
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword.confirm ? "text" : "password"}
+                name="confirm_password"
+                placeholder="Confirm Password"
+                value={formData.confirm_password}
+                onChange={handleChange}
+                className={`form-input pr-10 ${
+                  fieldErrors.confirm_password
+                    ? "border border-red-500 focus:ring-red-500"
+                    : ""
+                }`}
+              />
 
-  <button
-    type="button"
-    onClick={() =>
-      setShowPassword((prev) => ({
-        ...prev,
-        confirm: !prev.confirm,
-      }))
-    }
-    className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground"
-  >
-    {showPassword.confirm ? <EyeOff size={18} /> : <Eye size={18} />}
-  </button>
-
-</div>
-  {fieldErrors.confirm_password && (
-    <p className="text-red-500 text-xs mt-1">
-      {Array.isArray(fieldErrors.confirm_password)
-        ? fieldErrors.confirm_password[0]
-        : fieldErrors.confirm_password}
-    </p>
-  )}
+              <button
+                type="button"
+                onClick={() =>
+                  setShowPassword((prev) => ({
+                    ...prev,
+                    confirm: !prev.confirm,
+                  }))
+                }
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground"
+              >
+                {showPassword.confirm ? (
+                  <EyeOff size={18} />
+                ) : (
+                  <Eye size={18} />
+                )}
+              </button>
+            </div>
+            {fieldErrors.confirm_password && (
+              <p className="text-red-500 text-xs mt-1">
+                {Array.isArray(fieldErrors.confirm_password)
+                  ? fieldErrors.confirm_password[0]
+                  : fieldErrors.confirm_password}
+              </p>
+            )}
           </div>
 
           {/* Submit Button */}
           {/* Inline Message */}
-{message.text && (
-  <p
-    className={`text-sm text-center mb-2 ${
-      message.type === "success"
-        ? "text-green-600"
-        : "text-red-500"
-    }`}
-  >
-    {message.text}
-  </p>
-)}
+          {message.text && (
+            <p
+              className={`text-sm text-center mb-2 ${
+                message.type === "success" ? "text-green-600" : "text-red-500"
+              }`}
+            >
+              {message.text}
+            </p>
+          )}
 
           <div className="flex justify-center items-center">
             <button
